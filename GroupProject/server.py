190fa92108e6df92aae90281597f55c16766092c
D Lei <a0_0@Ds-MBP-2221.lan>
2023-10-18 20:42:58 -0400

    2

--- GroupProject/server.py
+++ GroupProject/server.py
@@ -1,163 +1,178 @@
+
 import bcrypt
 import secrets
-from flask_socketio import SocketIO
-from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from flask import Flask, render_template, request, redirect, url_for, flash, make_response, jsonify
-from html import escape
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, escape, jsonify
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+client = MongoClient("mongodb://mongo:27017/")
 
-    socketio = SocketIO(app)
+db = client["database"]         
+app.secret_key = "secret_Key"
 
-    app.secret_key = "secret_Key"
+# Stores usernames and passwords {'username': username_val, 'password': password_val} 
+user_db = db["user_db"]                             
 
-    app.config["MONGO_URI"] = "mongodb://mongo:27017/mydatabase"
-    mongo = PyMongo(app)
-    client = MongoClient("mongodb://mongo:27017/")
+# Stores authentication tokens {'token': token_val, 'username': username_val}
+auth_tokens = db["auth_tokens"]     
 
-    db = client["database"]        
+# Stores Post History {postId,username,title,description,likes}
+post_collection = db["post_collection"]
 
-    #Stores usernames and passwords {'username': username_val, 'password': password_val} 
-    user_db = db["user_db"]                             
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        # HTML injection, escape all HTML characters in the username.
+        user_name = escape(request.form['username'])
+        password = request.form['password']
 
-    #Stores authentication tokens {'token': token_val, 'username': username_val}
-    auth_tokens = db["auth_tokens"]     
+        # Check if a user name is already exists
+        existing_user = user_db.find_one({'name': user_name})
 
-    #Stores Post History {postId,username,title,description,likes}
-    post_collection = db["post_collection"]
-
-    @app.after_request
-    def noSniff(response):
-        response.headers['X-Content-Type-Options'] = 'nosniff'
-        return response
-
-    @app.route('/')
-    @app.route('/main')
-    def main():
-        posts = list(mongo.db.posts.find())
-        return render_template("index.html", posts=posts)
-
-    @app.route('/register', methods=['GET', 'POST'])
-    def register():
-        if request.method == 'POST':
-            # HTML injection, escape all HTML characters in the username.
-            user_name = escape(request.form['username'])
-            password = request.form['password']
-
-            # Check if a user name is already exists
-            existing_user = user_db.find_one({'name': user_name})
-
-            # Check username and password are filled
-            if not user_name or not password:
-                flash('Username or password cannot be empty')
-                return redirect(url_for('index'))
-
-            # Notify if the username is already taken
-            if existing_user:
-                flash('Username already exists')
-                return redirect(url_for('index'))
-
-            salt = bcrypt.gensalt()
-            hash_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-            user_db.insert_one({'name': user_name, 'password': hash_pass, 'salt': salt})
-            flash('Registration success')
+        # Check username and password are filled
+        if not user_name or not password:
+            flash('Username or password cannot be empty')
+            print("36")
             return redirect(url_for('index'))
 
-        return render_template('index.html')
+        # Notify if the username is already taken
+        if existing_user:
+            flash('Username already exists')
+            print("42")
+            return redirect(url_for('index'))
 
-    @app.route('/get-username')
-    # Displays username
-    def getUsername():
-        token = request.cookies.get('auth_token')
-        t = auth_tokens.find_one({'token': token})
-        if t:
-            current_username = t['username']
-        else: 
-            current_username = None
-        return jsonify({'username': current_username})
+        salt = bcrypt.gensalt()
+        hash_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        try:
-            username = request.form['username']
-            password = request.form['password']
-            user_data = user_db.find_one({"name": username})
+        user_db.insert_one({'name': user_name, 'password': hash_pass, 'salt': salt})
+        flash('Registration success')
+        return redirect(url_for('index'))
 
-            if user_data:
-                hashed_PW = bcrypt.hashpw(password.encode('utf-8'), user_data['salt'])
-                if hashed_PW == user_data['password']:
-                    token = secrets.token_hex(32)
-                    auth_tokens.insert_one({'token': token, 'username': username})
-                    response = make_response(render_template('login.html'))
-                    response.set_cookie("auth_token", token, max_age=3600, httponly=True)
-                    return response
-                else:
-                    response = "Invalid password."
-                    raise Exception()
+    return render_template('index.html')
+
+
+@app.route('/') 
+def index():
+    return render_template('index.html')
+
+@app.route('/get-username')
+# Displays username
+def getUsername():
+    token = request.cookies.get('auth_token')
+    t = auth_tokens.find_one({'token': token})
+    if t:
+        current_username = t['username']
+    else: 
+        current_username = None
+    return jsonify({'username': current_username})
+
+@app.route('/static/functions.js')
+def functions():
+    file = open("static/functions.js",encoding="utf-8")
+    file = file.read()
+    response = make_response(file)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.mimetype = "text/javascript"
+    return response
+
+@app.route('/static/style.css')
+def style():
+    file = open("static/style.css",encoding="utf-8")
+    file = file.read()
+    response = make_response(file)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.mimetype = "text/css"
+    return response
+
+@app.route('/favicon.ico')
+@app.route('/static/favicon.ico')
+def favicon():
+    file = open("static/favicon.ico","rb")
+    file = file.read()
+    response = make_response(file)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.mimetype = "image/ico"
+    return response
+
+@app.route('/visit-counter')
+def cookieCounter():
+    if "Cookie" not in request.headers:
+        response = make_response("Cookie Number is: 1")
+        response.mimetype = "text/plain"
+        response.set_cookie(key= "visits", value= "1", max_age= 3600)
+        return response
+    else:
+        splitCookieList = request.headers["Cookie"].split(";")
+        visitsString = splitCookieList[0]
+
+        splitVisitsString = visitsString.split("=")
+        visitsNum = int(splitVisitsString[-1])
+        visitsNum += 1
+        visitsNum = str(visitsNum)
+
+        response = make_response("Cookie Number is: " + visitsNum)
+        response.set_cookie(key= "visits", value=visitsNum, max_age = 3600)
+        return response
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    try:
+        print("110")
+        username = request.form['username']
+        password = request.form['password']
+        user_data = user_db.find_one({"name": username})
+        print("123")
+        if user_data:
+            hashed_PW = bcrypt.hashpw(password.encode('utf-8'), user_data['salt'])
+            if hashed_PW == user_data['password']:
+                token = secrets.token_hex(32)
+                auth_tokens.insert_one({'token': token, 'username': username})
+                response = make_response(render_template('login.html'))
+                response.set_cookie("auth_token", token, max_age=3600, httponly=True)
+                return response
             else:
-                response = "Username does not exist."
+                response = "Invalid password."
+                print("136")
                 raise Exception()
-
-        except Exception:
-            return response, 400
-    
-    @app.route('/post-history')
-    def post_history():
-        posts = list(mongo.db.posts.find())
-        for post in posts:
-            post['_id'] = str(post['_id'])
-        return jsonify(posts)
-
-    @app.route('/post-likes')
-    def likeFunction(postId):
-        post = post_collection.find({"postId" : postId})
-        return
-
-    @app.route('/post-submission')
-    def submitPost():
-        username = ""
-
-        if request.cookies['auth_token']:
-            auth_token = request.cookies['auth_token']
-            for userInfo in user_db.find({}):
-                if bcrypt.checkpw(auth_token.encode(),userInfo['auth_token']):
-                    username = userInfo['username']
-                    break
-        
         else:
-            response = "Not Logged In"
-            make_response(response)
-            return response
+            response = "Username does not exist."
+            print("140")
+            raise Exception()
 
-        title = request.form['title']
-        description = request.form['description']
-        post_collection.insert_one({"username" : username,"title" : title, "description" : description, "likes" : 0})
-        #Clear the Submission Sheet after and send a message saying Post was sent!
-        return
+    except Exception:
+        return response, 400
+    
+@app.route('/post-history')
+def post_history():
+    posts = list(post_collection.find({}))
+    for post in posts:
+        post['_id'] = str(post['_id'])
+    return jsonify(posts)
 
-    @socketio.on('submit_post')
-    def handle_submit_post(data):
-        username = data.get('username', 'Anonymous')  #Use the username from the data or default to 'Anonymous'
-        title = data['title']
-        description = data['description']
+@app.route('/post-likes', methods=['POST'])
+def likeFunction(postId):
+    post = post_collection.find({"postId" : postId})
+    return
 
-        #Save to MongoDB
-        post_id = mongo.db.posts.insert_one({'username': username, 'title': title, 'description': description}).inserted_id
+@app.route('/post-submission', methods=['POST'])
+def submitPost():
+    username = ""
+    auth_token = request.cookies.get('auth_token')
+    document = auth_tokens.find_one({'token': auth_token})
+    if document:
+        username = document['username']
+    else:
+        response = "Not Logged In"
+        make_response(response)
+        return response
+    print("168 request", request)
+    title = request.form.get('title', "")
+    description = request.form.get('description', "")
+    post_collection.insert_one({"username" : username,"title" : title, "description" : description, "likes" : 0})
+    #Clear the Submission Sheet after and send a message saying Post was sent!
+    flash('Post submitted successfully!')
+    return redirect(url_for('index'))
 
-        data['id'] = str(post_id)
-        
-        print("submit post")
-        #Send the post to all clients
-        socketio.emit('new_post', data, room='broadcast')
-
-    return app, socketio  # Return both app and socketio
-app, socketio = create_app()
-
-if __name__ == '__main__':
-    host = "0.0.0.0"
-    port = 8080
-    socketio.run(app, host=host, port=port, debug=True, allow_unsafe_werkzeug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- GroupProject/static/functions.js
+++ GroupProject/static/functions.js
@@ -1,23 +1,11 @@
-//This object represents a post and contains 
-//the username of the sender, the title of the post, the description of the post, and a unique identifier for the post.
-//postJSON: A JavaScript object with properties username, title, description, and id.
-const socket = io.connect('http://localhost:8080/');
-socket.on('new_post', function(data) {
-    addPost(data);
-});
-
+//Creating the HTML for each Post
 function postHTML(postJSON) {
     const username = postJSON.username;
     const title = postJSON.title;
     const description = postJSON.description;
     const postId = postJSON.id;
-
-    //<br>: Inserts a line break. This ensures that each post appears on a new line.
-
-    //>X</button> This is an HTML button. \
-    //The text inside the button is "X", suggesting that it's a "delete" button.
-
-    let postHTML = "<br><span id='post_" + postId + "'>";
+    
+    let postHTML = "<div class='post-box' id='post_" + postId + "'>";
 
     //Display the username in bold.
     postHTML += "<b>" + username + "</b>: ";
@@ -28,60 +16,13 @@ function postHTML(postJSON) {
     //Display the description.
     postHTML += "<strong>Description:</strong> " + description;
 
-    postHTML += "</span>";
+    //Display the Like/Dislike Buttons and the Counter for Likes
+    postHTML += /* "<br>" + likeCounter + */ "<button onclick='likePost(\"" + postId + "\")'>LIKE</button>" 
+
+    postHTML += "</div>";
 
     return postHTML;
-}
 
-function clearPost() {
-    const posts = document.getElementById("postHistory");
-    posts.innerHTML = "";
-}
-
-function addPost(postJSON) {
-    //This element presumably contains all the posts on the webpage.
-    const posts = document.getElementById("postHistory");
-    //Append the new post to the existing content inside the "postHistory" element
-    //postHTML converts postJSON object into a formatted HTML string.
-    posts.innerHTML += postHTML(postJSON);
-    //Scroll to the top
-    posts.scrollIntoView(true);
-    posts.scrollTop = 0;
-}
-
-//Send a post from the client side to the server.
-function sendPost() {
-    //Retrieve the title and description input values
-    const postTitleBox = document.getElementById("post-title-box");
-    const postDescriptionBox = document.getElementById("post-description-box");
-    const title = postTitleBox.value;
-    const description = postDescriptionBox.value;
-    const xsrfToken = document.getElementById("xsrf-token");
-    //After retrieving the post, the text box is cleared so the user can type a new title and description.
-    postTitleBox.value = "";
-    postDescriptionBox.value = "";
-    socket.emit('submit_post', { title: title, description: description });
-    postTitleBox.focus();
-}
-
-//Fetch the latest post history from the server and display it on the client side. 
-function updatePost() {
-    //Allow the function to make an asynchronous request to the server.
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            //Remove all current post from the display, in preparation for displaying the new post.
-            clearPost();
-            const posts = JSON.parse(this.response);
-            //This loop iterates over each post in the parsed posts array. 
-            //For each post, the addPost function is called to display it on the client side.
-            for (const post of posts) {
-                addPost(post);
-            }
-        }
-    }
-    request.open("GET", "/post-history");
-    request.send();
 }
 
 //Adding a like
@@ -96,14 +37,37 @@ function likePost(postId) {
     request.send();
 }
 
-function welcome() {
-    document.addEventListener("keypress", function (event) {
-        if (event.code === "Enter") {
-            sendPost();
-        }
-    });
+//Adding the postHTML on indexHTML
+function addPostToPosts(messageJSON) {
+    const posts = document.getElementById("postHistory");
+    posts.innerHTML += postHTML(messageJSON);
+}
 
-    document.getElementById("post-title-box").focus();
-    setInterval(updatePost, 2000)
-    //updatePost()
+//Updates all Posts
+function updatePosts() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            clearPosts();
+            const posts = JSON.parse(this.response);
+            for (const post of posts) {
+                addPostToPosts(post);
+            }
+        }
+    }
+
+    request.open("GET", "/post-history");
+    request.send();
+}
+
+//Clearing Posts
+function clearPosts() {
+    const posts = document.getElementById("postHistory");
+    posts.innerHTML = "";
+}
+
+//Constantly Calls updatePosts() on startup
+function welcome() {
+    updatePosts();
+    setInterval(updatePosts,2000);
 }
